170c3137a1c662d636b6a1796a1d71a2a1466081
Aran Moncusí Ramírez <aran.moncusi@wallapop.com>
2021-09-07 01:20:23 +0200

    Add java installation process

--- user_data.sh
+++ user_data.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env sh
 
 echo "Starting user_data.sh script"
+
+echo "Install common packages"
+
+apt -y -q update
+
+apt install -y -q software-properties-common openjdk-16-jdk-headless
+
+echo "Common packages installed"
+
+java -version
